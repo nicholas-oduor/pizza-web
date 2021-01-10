f225b5f66030b955bbeeaebee17140e0f6a7e1be
nicholas-oduor <oduor5742@gmail.com>
2021-01-10 10:42:14 +0300

    added crust  function

--- scripts.js
+++ scripts.js
@@ -10,10 +10,35 @@ let totalAmount = 0;
 
     Pizza.prototype.myToppingsPrice = function () {
         if (this.size === "large") {
-            if (this.toppings === "tomato") {
+            if (this.toppings === "pancetta") {
                 return 150;
-            } else if (this.toppings === "onions") {
+            } else if (this.toppings === "mushroom") {
                 return 120;
             } else {
                 return 180;
             }
+        } else if (this.size === "medium") {
+            if (this.toppings === "pancetta") {
+                return 100;
+            } else if (this.toppings === "mushroom") {
+                return 80;
+            } else {
+                return 140;
+            }
+        } else {
+            if (this.topping === "pancetta") {
+                return 80;
+            } else if (this.toppings === "mushroom") {
+                return 50;
+            } else {
+                return 100;
+            }
+        }
+    };
+    Pizza.prototype.getCrustPrice = function () {
+        if (this.crust === "pepperoni") {
+            return 100;
+        } else {
+            return 0;
+        }
+    };
