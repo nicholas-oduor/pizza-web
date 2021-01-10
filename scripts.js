4c3fb7ba38c28d89e2cad4275e43adb6e79a78d6
nicholas-oduor <oduor5742@gmail.com>
2021-01-10 10:33:08 +0300

    adding toppings prices on a large pizza

--- scripts.js
+++ scripts.js
@@ -5,3 +5,15 @@ function Pizza(type, crust, toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
+let totalAmount = 0;
+    let newOrder = [];
+
+    Pizza.prototype.myToppingsPrice = function () {
+        if (this.size === "large") {
+            if (this.toppings === "tomato") {
+                return 150;
+            } else if (this.toppings === "onions") {
+                return 120;
+            } else {
+                return 180;
+            }
