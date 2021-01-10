8c3900126570f278002cea9655265b8fd5612cb7
nicholas-oduor <oduor5742@gmail.com>
2021-01-10 09:53:33 +0300

    added license.MD

---
+++ css/bootstrap/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+function Pizza(type, crust, toppings, size) {
+    this.type = type;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.size = size;
+}
